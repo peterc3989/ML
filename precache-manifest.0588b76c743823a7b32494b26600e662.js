f5ea078e91b6e94c9b525129aa522791c3801b4d
Peter <peterc3989@gmail.com>
2020-09-11 15:36:46 -0700

    Updates

---
+++ precache-manifest.0588b76c743823a7b32494b26600e662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d6b22e7728af5f3b7cb5218ddf47a3",
+    "url": "/ML/index.html"
+  },
+  {
+    "revision": "d5084fbae7074a57ad2b",
+    "url": "/ML/static/css/main.c9ac0daa.chunk.css"
+  },
+  {
+    "revision": "c8cb45a83693ec286ef0",
+    "url": "/ML/static/js/2.8ac47d4c.chunk.js"
+  },
+  {
+    "revision": "d177d8c0a3c2e0f50ed86f968238c7be",
+    "url": "/ML/static/js/2.8ac47d4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5084fbae7074a57ad2b",
+    "url": "/ML/static/js/main.8bc279b5.chunk.js"
+  },
+  {
+    "revision": "7b2e8df40161cae61cdc",
+    "url": "/ML/static/js/runtime-main.9c4928ef.js"
+  }
+]);
